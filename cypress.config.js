f49a85bbb5079f90634b61b2a98825fef1d17d6c
archithdisha-cry <archithdisha@gmail.com>
2025-11-10 13:55:40 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     specPattern: "tests/cypress/e2e/**/*.cy.js",
-    baseUrl: "http://localhost:3000"
+    baseUrl: "http://localhost:3000",
+    supportFile: false
   }
 });
