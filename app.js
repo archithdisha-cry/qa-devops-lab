ceeb01ec3bde5e6c7f4e5463260b844d00b0e1a2
archithdisha-cry <archithdisha@gmail.com>
2025-11-10 13:59:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
+
 app.get('/', (req, res) => res.send('Flexitive-style QA DevOps demo running!'));
 app.listen(3000, () => console.log('App running on port 3000'));
